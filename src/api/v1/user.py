806c00bbba981787dccacfab567fcaffcab4b642
root <root@89-108-76-218.cloudvps.regruhosting.ru>
2022-11-10 12:10:47 +0300

    Add roles to JWT-payload

--- src/api/v1/user.py
+++ src/api/v1/user.py
@@ -154,16 +154,19 @@ class Login(Resource):
             auth_hist = Authentication(user_id=user.id, user_agent=user_agent)
             db.session.add(auth_hist)
             db.session.commit()
-            # сохранять refresh-токен в базе
+
             jti_refresh = get_jti(refresh_token)
+            roles = [str(role.id) for role in user.roles]
             additional_claims = {
                 "jti_refresh": jti_refresh,
                 "user_uuid": user.id,
-                "is_administrator": user.is_administrator
+                "is_administrator": user.is_administrator,
+                "roles": roles
             }
             access_token = create_access_token(identity=user.id,
                                                additional_claims=additional_claims)
             jwt_redis_refresh.set(get_jti(refresh_token), str(user.id), ex=REFRESH_EXPIRES)
+
             return {"access_token": access_token,
                     "refresh_token": refresh_token,
                     "user_id": str(user.id)}, HTTPStatus.OK
@@ -218,10 +221,12 @@ class RefreshTokens(Resource):
                                                      "user_uuid": user.id
                                                  })
             jti_refresh = get_jti(refresh_token)
+            roles = [str(role.id) for role in user.roles]
             additional_claims = {
                 "jti_refresh": jti_refresh,
                 "user_uuid": user.id,
-                "is_administrator": user.is_administrator
+                "is_administrator": user.is_administrator,
+                "roles": roles
             }
             access_token = create_access_token(identity=identity,
                                                fresh=False,
