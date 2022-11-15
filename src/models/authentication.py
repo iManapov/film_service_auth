98b214b9c2375d80e2ebc8e5285307cd993fb4d3
iManapov <manapovilnar@gmail.com>
2022-11-15 21:33:32 +0300

    Added 'authentication' table partitions

--- src/models/authentication.py
+++ src/models/authentication.py
@@ -1,26 +1,111 @@
 import uuid
 
-from sqlalchemy import ForeignKeyConstraint, Index, ForeignKey
+from sqlalchemy import event, ForeignKey, DDL, UniqueConstraint
+
 from sqlalchemy.dialects.postgresql import UUID
 from datetime import datetime
 from src.db.db_postgres import db
+from sqlalchemy.ext.declarative import declared_attr
 
 
-class Authentication(db.Model):
-    __tableName__ = "authentication"
-
-    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
-    user_id = db.Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
+class AuthenticationMixin:
+    @declared_attr
+    def user_id(self):
+        return db.Column(
+            UUID(as_uuid=True), ForeignKey("users.id", ondelete="CASCADE")
+        )
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4,  nullable=False)
     user_agent = db.Column(db.String, nullable=True)
-    datetime = db.Column(db.DateTime, default=datetime.now)
+    login_at = db.Column(db.DateTime, default=datetime.now, primary_key=True)
 
+    def __repr__(self) -> str:
+        return f"<Login history {self.id}>"
+
+    def as_value(self) -> str:
+        return self.event_type.value
+
+
+class Authentication(AuthenticationMixin, db.Model):
+    __tableName__ = "authentication"
     __table_args__ = (
-        Index("authentication_user_id_index", user_id),  # composite index on user_id
+        UniqueConstraint("login_at", "id"),
+        {'postgresql_partition_by': 'RANGE (login_at)'},
     )
 
     @classmethod
     def get_login_history(cls, user_id, page, size):
-        return cls.query.filter_by(user_id=user_id).order_by(cls.datetime.desc()).paginate(page=page, per_page=size)
+        return cls.query.filter_by(user_id=user_id).order_by(cls.login_at.desc()).paginate(page=page, per_page=size)
 
     def as_dict(self) -> dict:
         return {c.name: str(getattr(self, c.name)) for c in self.__table__.columns}
+
+
+class Authentication2020(AuthenticationMixin, db.Model):
+    __tablename__ = 'authentication2020'
+
+
+class Authentication2021(AuthenticationMixin, db.Model):
+    __tablename__ = 'authentication2021'
+
+
+class Authentication2022(AuthenticationMixin, db.Model):
+    __tablename__ = 'authentication2022'
+
+
+class Authentication2023(AuthenticationMixin, db.Model):
+    __tablename__ = 'authentication2023'
+
+
+class Authentication2024(AuthenticationMixin, db.Model):
+    __tablename__ = 'authentication2024'
+
+
+PARTITION_TABLES_REGISTRY = (
+    (Authentication2020, "('2020-01-01') TO ('2021-01-01')"),
+    (Authentication2021, "('2021-01-01') TO ('2022-01-01')"),
+    (Authentication2022, "('2022-01-01') TO ('2023-01-01')"),
+    (Authentication2023, "('2023-01-01') TO ('2024-01-01')"),
+    (Authentication2024, "('2024-01-01') TO ('2025-01-01')")
+)
+
+
+def create_table_login_history_partition_ddl(
+    table: str, periods: str
+) -> None:
+    return DDL(
+        """
+        ALTER TABLE authentication ATTACH PARTITION %s FOR VALUES FROM %s;"""
+        % (table, periods)
+    ).execute_if(dialect="postgresql")
+
+
+def attach_event_listeners() -> None:
+    for class_, periods in PARTITION_TABLES_REGISTRY:
+        class_.__table__.add_is_dependent_on(Authentication.__table__)
+        event.listen(
+            class_.__table__,
+            "after_create",
+            create_table_login_history_partition_ddl(
+                class_.__table__, periods
+            ),
+        )
+
+
+attach_event_listeners()
+
+'''
+Для применения миграций, необходимо добавить следующий код после миграций alembic:
+
+from src.models.authentication import PARTITION_TABLES_REGISTRY, create_table_login_history_partition_ddl
+.
+.
+.
+def upgrade():
+    .
+    .
+    .
+    # ### end Alembic commands ###
+    for table_class, device_type in PARTITION_TABLES_REGISTRY:
+        ddl = create_table_login_history_partition_ddl(table_class.__table__, device_type)
+        ddl(target=None, bind=op.get_bind())
+'''
