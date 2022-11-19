f46c674ec9b70f5933e629e0bc9f617904660d7e
iManapov <manapovilnar@gmail.com>
2022-11-19 12:35:03 +0300

    Added enable tracing config

--- src/app.py
+++ src/app.py
@@ -9,11 +9,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flasgger import Swagger
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 
-from src.core.oauth_config import yandex, google, vk, mail
+# from src.core.oauth_config import yandex, google, vk, mail
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
+from src.core.oauth_config import yandex, google, vk, mail
 from src.extensions import ma, jwt, migrate, limiter
 from src.db.db_postgres import db, init_db
 from src.models.authentication import Authentication
@@ -24,9 +25,7 @@ from src.utils.create_user import init_create_user
 from src.utils.tracing import configure_tracer
 
 
-configure_tracer()
 app = Flask(__name__)
-FlaskInstrumentor().instrument_app(app)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 # Конфигурация Flask-JWT-Extended
 app.config["PROPAGATE_EXCEPTIONS"] = True
@@ -77,6 +76,9 @@ def register_extensions(app):
     migrate.init_app(app, db)
     limiter.init_app(app)
     init_create_user(app)
+    if settings.enable_tracer:
+        configure_tracer()
+        FlaskInstrumentor().instrument_app(app)
 
 
 def prepare_start():
